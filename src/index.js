0f43ef22e6faf488b8d8c0c1b2501282acfa1188
ashabahmed <ashabahmed7@gmail.com>
2020-10-06 18:50:10 -0400

    copy pasted code from lecture and will review over.

--- src/index.js
+++ src/index.js
@@ -1,39 +1,54 @@
-document.addEventListener("DOMContentLoaded", function(){
-  // your code here
-  const toDoList = {};
+document.addEventListener("DOMContentLoaded", () => {
 
-  toDoList.tasks = document.getElementById("new-task-description").value;
+  function submitHandler() {
+    const taskForm = document.querySelector('#create-task-form')
+    taskForm.addEventListener('submit', function(e){
+      e.preventDefault()
+      const form = e.target
+      // const task = form[0].value
+      const task = form["new-task-description"].value
+      const priority = form.priority.value
 
-  const button = document.querySelector('form').lastElementChild
   
-  button.addEventListener('click', function(){
-    const newTasks = document.getElementById("tasks")
-    const tasksLi = document.createElement('li')
-    tasksLi.innerHTML = toDoList.tasks 
-    newTasks.appendChild(tasksLi)
-    console.log(tasksLi)
-  });
+      const taskLi = document.createElement('li')
+      taskLi.textContent = task
 
+      const deleteButton = document.createElement('button')
+      deleteButton.textContent = "x"
+      deleteButton.classList.add('delete')
+      taskLi.append(deleteButton)
+
+      // deleteButton.addEventListener('click', function(e){
+      //   e.target.parentElement.remove()
+      // })
+  
+  
+      // const taskList = document.querySelector('#tasks')
+      // taskList.append(taskLi)
+      
+      const div = document.querySelector('#' + priority)
+      div.append(taskLi)
+  
+      form.reset()
+    })
+  }
+
+  function clickHandler() {
+    document.addEventListener('click', function(e){
+      if(e.target.matches('.delete')){
+        e.target.parentElement.remove()
+      }
+    })
+  }
+  
+  submitHandler()
+  clickHandler()
 });
 
 
-
-// const newTasks = document.getElementById("tasks")
-
-// function createTasks(taskInput) {
-  
-//   const tasksLi = document.createElement('li')
-  
-//   newTasks.appendChild(tasksLi)
-
-// }
-
-
-
-
-
-// 1. When we submit the input from form as a const
-// 2. We want to have our const into the tasks id  in li form
-// 3. We would have to select the div(id=list) and the ul(id=tasks)
-// document.getElementById("tasks")
-
+/* As a user, the task string that I provided should appear on the DOM after the submit button has been activated. 
+1. add a submit listener to the form
+2. on submit, squash the default behavior
+3. get the data out of the form
+4. create a new li with the data and slap it on the DOM
+*/
