e123eb96dd59de2e0c7a40c5982a2a6cced193b2
Sean LaFlam <sean.laflam@gmail.com>
2020-10-10 10:20:52 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function(){
   // your code here
-});
+
+  const form = document.querySelector('#create-task-form')
+  console.log(form)
+  form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    const newTasks = document.getElementById("tasks")
+    const tasksLi = document.createElement('li')
+    const deleteBtn = document.createElement('button')
+    deleteBtn.innerText = 'x'
+    deleteBtn.classList.add("delete")
+    const task = document.getElementById("new-task-description").value;
+    tasksLi.innerHTML = task
+    const priority = document.getElementById("priorityList").value;
+    tasksLi.classList.add(priority)
+    newTasks.append(tasksLi)
+    tasksLi.append(deleteBtn)
+    form.reset()
+  });
+
+  document.addEventListener('click', function(e){
+    if (e.target.className === 'delete'){
+      e.target.parentNode.remove()
+    }
+  })
+
+})
